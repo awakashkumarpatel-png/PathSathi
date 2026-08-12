ec5ef0b0cf7111b4ffd2040ad11399eee5ec30f1
awakashkumarpatel-png <awakashkumarpatel@gmail.com>
2026-08-12 17:11:21 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling-preview)
+    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 }
-
